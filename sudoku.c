693348bad9f8a35c984f1cd4dc9cd99f8d55d95e
Freezing128 <59934306+Freezing128@users.noreply.github.com>
2020-02-26 21:47:22 +0100

    Updated sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,97 +1,116 @@
 /*
-* Sudoku solver v1.0
+* Sudoku solver v1.1
 * @Author: Freezing
-* @Date: 14/01/2020
+* @Date: 03/02/2020
 */
+
 #include <stdio.h>
 #include <stdlib.h> 
 
 #define MAX_COLS 9
 #define MAX_ROWS 9
 
-#define ERROR_TOO_LONG_INPUT -1
-#define ERROR_WRONG_INPUT 0
-#define SUCCESSFULLY_READ_INPUT 1
+#define FILE_INPUT "sudoku_in.txt"
+#define FILE_OUTPUT "sudoku_out.txt"
 
-typedef struct t_board 
-{
-    char position [MAX_ROWS] [MAX_COLS];
-} board;
+#define FILE_ERROR 1
+#define INPUT_ERROR 2
+
 /*
-* Reads input from stdin
+* Reads input from file
 */
-int readInput ( board * gameBoard )
+int readInput ( char sudoku_board [MAX_ROWS][MAX_COLS], FILE * fs, int * filledNumbers )
 {
+    int err;
     int row = 0;
     int col = 0;
-    int err;
     char c;
-    printf("Insert sudoku board:\n");
-    while ( 42 )
+    while ( row < MAX_ROWS )
     {
-        if ( row > MAX_ROWS - 1 )
-            return ERROR_TOO_LONG_INPUT;
-        if ( ( err = scanf ( "%c", &c ) ) != 1 || 
-        ( c != '1' && c != '2' &&  c != '3' && c != '4' && 
-          c != '5' && c != '6' &&  c != '7' && c != '8' && 
-          c != '9' && c != '*' && c != '\n' ) )
+        if ( (err = fscanf (fs, "%c", &c)) != 1 || ( ( c < '1' || c > '9' ) && c != '\n' && c != '*' ) )
         {
-            if ( feof ( stdin ) )
+            if ( err == EOF ) {
                 break;
-            return ERROR_WRONG_INPUT;
-        }
+            } else {
+                return 0;
+            }
+        }         
         if ( col == MAX_COLS )
         {
-            row++;
+            if ( c != '\n' ) {
+                return 0;
+            }
             col = 0;
+            row++;
         }
-        if ( c != '\n' )
+        else
         {
-            gameBoard -> position [row] [col] = c;
+            if ( c == '\n' ) {
+                return 0;
+            }
+            if ( c >= '1' && c <= '9' ) {
+                (*filledNumbers)++;
+            }
+            sudoku_board [row] [col] = c;
             col++;
         }
     }
-    if ( row != MAX_ROWS - 1 || col != MAX_COLS )
-      return ERROR_WRONG_INPUT;
-    return SUCCESSFULLY_READ_INPUT;
+    if ( ( row < MAX_ROWS && col != MAX_COLS ) || ( row == MAX_ROWS && col != 0 ) ) {
+        return 0;
+    }
+    return 1;
 }
 /*
-* Prints the board on stdout 
+* Prints out sudoku board
 */
-void printBoard ( board gameBoard )
+void printBoard ( char sudoku_board [MAX_ROWS][MAX_COLS], FILE * fw )
 {
     int j = 0;
     for ( int i = 0; i < MAX_ROWS; i ++ )
     {
-        if ( i % 3 == 0 )
+        if ( i % 3 == 0 ) {
+            fprintf ( fw, "+==========================+\n" );
             printf ( "+==========================+\n" );
+        }
         for ( ; j < MAX_COLS; j++ )
         {
-            if ( j == 0 )
+            if ( j == 0 ) {
+                fprintf ( fw, "| " );
                 printf ( "| " );
-            else if ( j % 3 == 0 )
+            } else if ( j % 3 == 0 ) {
+                fprintf ( fw, " | " );
                 printf ( " | " );
-            printf ( "%c ", gameBoard . position[i][j] );
-            if ( j == 8 )
+            }
+            fprintf ( fw, "%c ", sudoku_board [i] [j] );
+            printf ( "%c ", sudoku_board [i] [j] );
+            if ( j == 8 ) {
+                fprintf ( fw, " | " );
                 printf ( " | " );
+            }
         }
-        if ( i == 8 )
+        if ( i == 8 ) {
+            fprintf ( fw, "\n+==========================+\n" );
             printf ( "\n+==========================+\n" );
-        else
+        } else {
+            fprintf ( fw, "\n" );
             printf ( "\n" );
+        }
         j = 0;
     }
 }
 /*
 * Horizontal check
 */
-int isInRow ( board * gameBoard, int row, int number )
+int isInRow ( char sudoku_board [MAX_ROWS][MAX_COLS], int row, int col, int number, int validityCheck )
 {
 	for ( int i = 0; i < MAX_ROWS; i++ )
 	{
-		if ( gameBoard -> position [row] [i] == number + '0' )
-		{
-			return 1;
+		if ( sudoku_board [row] [i] == number + '0' ) {
+            if ( validityCheck == 1 && col == i ) {
+                continue;
+            } else {
+			    return 1;
+            }
 		}
 	}
 	return 0;
@@ -99,13 +118,16 @@ int isInRow ( board * gameBoard, int row, int number )
 /*
 * Vertical check
 */
-int isInCol ( board * gameBoard, int col, int number )
+int isInCol ( char sudoku_board [MAX_ROWS][MAX_COLS], int row, int col, int number, int validityCheck )
 {
 	for ( int i = 0; i < MAX_COLS; i++ )
 	{
-		if ( gameBoard -> position [i] [col] == number + '0' )
-		{
-			return 1;
+		if ( sudoku_board [i] [col] == number + '0' ) {
+            if ( validityCheck == 1 && row == i ) {
+                continue;
+            } else {
+			    return 1;
+            }
 		}
 	}
 	return 0;
@@ -113,7 +135,7 @@ int isInCol ( board * gameBoard, int col, int number )
 /*
 * Box check
 */
-int isInBox ( board * gameBoard, int row, int col, int n )
+int isInBox ( char sudoku_board [MAX_ROWS][MAX_COLS], int row, int col, int n, int validityCheck )
 {
     int r = row - row % 3;
     int c = col - col % 3;
@@ -121,42 +143,47 @@ int isInBox ( board * gameBoard, int row, int col, int n )
     {
         for ( int j = c; j < c + 3; j++ )
         {
-            if ( gameBoard -> position [i] [j] == n + '0' )
-                return 1;
+            if ( sudoku_board [i] [j] == n + '0' ) {
+                if ( validityCheck == 1 && row == i && col == j ) {
+                    continue;
+                } else {
+			        return 1;
+                }
+            }
         }
-    }
+    } 
     return 0;
 }
 /*
-* Checks if the number could be filled into desired spot on the board or not
+* Checks if given number could be filled into desired slot or not
 */
-int isAllowed ( board * gameBoard, int row, int col, int n )
+int isAllowed ( char sudoku_board [MAX_ROWS][MAX_COLS], int row, int col, int n, int validityCheck )
 {
-    return ! ( isInRow ( gameBoard, row, n ) || isInCol ( gameBoard, col, n ) || isInBox ( gameBoard, row, col, n ) );
+    return ! ( isInRow ( sudoku_board, row, col, n, validityCheck ) 
+            || isInCol ( sudoku_board, row, col, n, validityCheck ) 
+            || isInBox ( sudoku_board, row, col, n, validityCheck ) );
 }
 /*
-* Recursive function used for finding the actual solution of given sudoku problem
+* Recursive function to find actual sudoku solution
 */
-int sudoku ( board * gameBoard )
+int sudokuRec ( char sudoku_board [MAX_ROWS][MAX_COLS] )
 {
 	for ( int row = 0; row < MAX_ROWS; row++ )
 	{
 		for ( int col = 0; col < MAX_COLS; col++ )
 		{
-			if ( gameBoard -> position [row] [col] == '*' )
+			if ( sudoku_board [row] [col] == '*' )
 			{
 				for ( int number = 1; number <= 9; number++ )
 				{			       
-					if ( isAllowed ( gameBoard, row, col, number ) )
+					if ( isAllowed ( sudoku_board, row, col, number, 0 ) )
 					{	
-						gameBoard -> position [row] [col] = number + '0';
-						if ( sudoku ( gameBoard ) )
-						{			     
+						sudoku_board [row] [col] = number + '0';
+						if ( sudokuRec ( sudoku_board  ) ) {			     
 							return 1;
 						}
-						else
-						{			        
-						    gameBoard -> position [row] [col] = '*';
+						else {			        
+						    sudoku_board [row] [col] = '*';
 						}
 					}
 				}
@@ -166,29 +193,109 @@ int sudoku ( board * gameBoard )
 	}
 	return 1;
 }
-/* Main function */
+/*
+* Checks given sudoku board if it's solvable or not (looking for duplicit numbers in row, column or box)
+*/
+int isValidSudoku ( char sudoku_board [MAX_ROWS][MAX_COLS] )
+{
+    int j = 0;
+    char num;
+    for ( int i = 0; i < MAX_ROWS; i++ )
+    {
+        for ( ; j < MAX_COLS; j++ )
+        {
+            if ( sudoku_board [i] [j] != '*' )
+            {
+                num = sudoku_board [i] [j];
+                if ( ! isAllowed (sudoku_board, i, j, num - '0', 1 ) ) {
+                    return 0;
+                }
+            }
+        }
+        j = 0;
+    }
+    return 1;
+}
+/*
+* Wrapper function for solving the sudoku
+*/
+int solveSudoku ( char sudoku_board [MAX_ROWS][MAX_COLS] )
+{
+    if ( ! isValidSudoku ( sudoku_board ) || ! sudokuRec ( sudoku_board ) ) {
+        return 0;
+    }
+    return 1;
+}
+/*
+* Opening files
+*/
+void openFiles ( FILE ** fs, FILE ** fw, int * flag )
+{
+    if ( ! ((*fs) = fopen (FILE_INPUT, "r")) ) { 
+        printf ( "File containing sudoku %s could not be opened!\n", FILE_INPUT );
+        (*flag)++;
+    }
+    if ( ! ((*fw) = fopen (FILE_OUTPUT, "w")) ) 
+    { 
+        printf ( "The file %s, where the solved sudoku is to be written, could not be opened!\n", FILE_OUTPUT );
+        if ( fclose(*fs) != 0 ) {
+            printf ( "Error while closing file%s\n", FILE_INPUT );  
+        }
+        (*flag)++;
+    }
+}
+/*
+* Closing files
+*/
+void closeFiles ( FILE * fs, FILE * fw )
+{
+    if ( fclose(fs) != 0 ) {
+        printf ( "Error while closing file %s\n", FILE_INPUT );
+    }
+    if ( fclose(fw) != 0 ) {
+        printf ( "Error while closing file %s\n", FILE_OUTPUT );
+    }
+}
+
+/* Main */
 int main()
 {
-    board gameBoard;
-    int error = readInput ( &gameBoard );
-    int numberOfResults = 0;
-    if ( error == ERROR_WRONG_INPUT )
-    {
-        printf ( "Wrong input.\n" );
-        return -1;
+    char sudoku_board [MAX_ROWS] [MAX_COLS];
+    FILE * fs,
+         * fw;
+    int filledNumbers = 0;
+    int flag = 0;
+
+    openFiles ( &fs, &fw, &flag );
+    if ( flag != 0 ) {
+        return FILE_ERROR;
     }
-    else if ( error == ERROR_TOO_LONG_INPUT )
+
+    printf ( "Insert sudoku:\n" );
+    if ( ! (readInput ( sudoku_board, fs, &filledNumbers )) )
     {
-        printf ( "Too long input.\n" );
-        return -2;
+        printf ( "Wrong input - forbidden characters or input is shorter/longer that it should be.\n" );
+        return INPUT_ERROR;
     }
-    printf ( "Inserted sudoku:\n" );
-    printBoard ( gameBoard );
-    printf ( "___________________________________\nCalculated soluton:\n___________________________________\n" );
-    if ( ! sudoku ( &gameBoard ) )
+
+    fprintf ( fw, "Given sudoku:\n" );
+    printf ( "Given sudoku:\n" );
+    printBoard ( sudoku_board, fw );
+    fprintf ( fw, "___________________________________\nSolved sudoku:\n___________________________________\n" );
+    printf ( "___________________________________\nSolved sudoku:\n___________________________________\n" );
+    if ( ! solveSudoku ( sudoku_board ) ) {
+        fprintf ( fw, "Solution does not exist.\n" );
         printf ( "Solution does not exist.\n" );
+    }
     else
-        printBoard ( gameBoard );
-    
+    {
+        if ( filledNumbers < 17 ) {
+            fprintf ( fw, "Sudoku could have more solutions, this is one of them:\n" );
+            printf ( "Sudoku could have more solutions, this is one of them:\n" );
+        }
+        printBoard ( sudoku_board, fw );
+    }
+
+    closeFiles ( fs, fw );
     return 0;
 }
